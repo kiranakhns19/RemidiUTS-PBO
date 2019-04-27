0e9ead61b4126e832e81fc86a3daf2532d35cee7
kiranakhns19 <49029418+kiranakhns19@users.noreply.github.com>
2019-04-27 23:38:32 +0700

    Update Komputer.java

--- src/com/modul/Komputer.java
+++ src/com/modul/Komputer.java
@@ -11,11 +11,11 @@ public class Komputer {
 
     public void memasang(CPU cpu) {
         this.cpu = cpu;
-        System.out.println("CPU : " + " " + this.cpu.getCPUData() + "dipasang");
+        System.out.println("CPU : " + " " + this.cpu.getCPUData() + " " + "dipasang");
     }
 
     public void mencabut(CPU cpu){
-        System.out.println("CPU : " + " " + this.cpu.getCPUData() +  "dicabut");
+        System.out.println("CPU : " + " " + this.cpu.getCPUData() + " " + "dicabut");
         this.cpu = null;
     }
 
