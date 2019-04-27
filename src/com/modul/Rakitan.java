2e4b2a15eb38e87f6c53ae6b2686de43e334a952
kiranakhns19 <49029418+kiranakhns19@users.noreply.github.com>
2019-04-27 23:37:34 +0700

    Update Rakitan.java

--- src/com/modul/Rakitan.java
+++ src/com/modul/Rakitan.java
@@ -3,9 +3,9 @@ package com.modul;
 public class Rakitan {
 
     public static void main(String[] args) {
-    Komputer komputer1 = new Komputer();
     Intel intel1 = new Intel(2);
     AMD amd1 = new AMD(3);
+    Komputer komputer1 = new Komputer(intel1);
 
 	komputer1.cetakInfo();
 	komputer1.mencabut(intel1);
